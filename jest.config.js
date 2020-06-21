0a789d2ae46a841df293843eecadd39dc7f9e137
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-21 11:56:40 -0300

    Configure coverage and tests

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,9 @@ module.exports = {
     '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/__mocks__/fileMock.ts',
   },
+  collectCoverage: true,
+  coverageDirectory: 'tests/coverage',
+  coverageReporters: ['text', 'lcov'],
+  testMatch: ['**/tests/**/*.spec.tsx'],
+  roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>/mocks/'],
 };
