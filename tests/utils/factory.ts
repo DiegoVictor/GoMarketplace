6d1bb186f353b735eeb7857705d1aa1343129df9
Diego Victor <diegovictorgonzaga@gmail.com>
2022-03-05 19:20:51 -0300

    Convert to number before use

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -8,8 +8,8 @@ factory.define(
     id: () => String(faker.datatype.number()),
     title: faker.name.title,
     image_url: faker.image.imageUrl,
-    price: faker.finance.amount,
-    quantity: () => faker.datatype.number({ min: 1, max: 3 }),
+    price: () => Number(faker.finance.amount()),
+    quantity: () => faker.datatype.number({ min: 2, max: 10 }),
   },
 );
 
