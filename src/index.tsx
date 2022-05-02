b07314fae81e02e089afc99f61f87632fb3c5586
Diego Victor <diegovictorgonzaga@gmail.com>
2022-05-02 15:38:47 -0300

    Create container

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import Routes from './routes';
 import AppContainer from './hooks';
+import { Container } from './styles';
 
 const App: React.FC = () => (
-  <View style={{ backgroundColor: '#312e38', flex: 1 }}>
+  <Container>
     <AppContainer>
       <StatusBar barStyle="light-content" backgroundColor="#312e38" />
       <Routes />
     </AppContainer>
-  </View>
+  </Container>
 );
 
 export default App;
