24f09974f46afd54d6ba547988fabbd2b90e4159
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-23 10:55:56 -0300

    Create reusable interfaces

---
+++ src/contracts/product.ts
@@ -0,0 +1,7 @@
+export interface IProduct {
+  id: string;
+  title: string;
+  image_url: string;
+  price: number;
+  quantity: number;
+}
---
+++ src/contracts/stack-list.ts
@@ -0,0 +1,4 @@
+export type StackParamList = {
+  Dashboard: undefined;
+  Cart: undefined;
+};
