37fcaf7c96eeb64306cd278ed664cf20376afa77
Diego Victor <diegovictorgonzaga@gmail.com>
2023-04-28 00:07:29 -0300

    Update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  preset: '@testing-library/react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/__mocks__/fileMock.ts',
-  },
+  clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'tests/coverage',
   coverageReporters: ['text', 'lcov'],
-  roots: ['<rootDir>/src/', '<rootDir>/tests/', '<rootDir>/mocks/'],
-  setupFiles: ['./src/jestSetupFile.js'],
+  preset: '@testing-library/react-native',
+  setupFilesAfterEnv: ['./src/jestSetupFile.ts'],
+  testEnvironment: 'node',
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native(-.*)?|@react-native(-community)?)/)',
   ],
