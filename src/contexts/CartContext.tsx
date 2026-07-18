d55e48ec6fd667f2c5ec96da2e6a1627ab28c079
DiegoVictor <diegovictorgonzaga@gmail.com>
2026-07-18 10:08:04 -0300

    Add CartContext and CartProvider for managing shopping cart state

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,39 @@
+import { IProduct } from '@/constants/product';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+
+export interface ICartContext {
+  products: IProduct[];
+  setProducts: (products: IProduct[]) => void;
+}
+
+const defaultValue: ICartContext = {
+  products: [],
+  setProducts: (products: IProduct[]) => {
+    defaultValue.products = products;
+  },
+};
+
+export const CartContext = createContext<ICartContext>(defaultValue);
+
+export const CartProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const cart = await AsyncStorage.getItem('cart');
+
+      if (cart) {
+        setProducts(JSON.parse(cart));
+      }
+    })();
+  }, []);
+
+  return (
+    <CartContext.Provider value={{ products, setProducts }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
